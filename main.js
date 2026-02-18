da09c531714bd6fafc1b2ee0fff0ca3443583aae
Akansha Wajge <akanshad.wajge@gmail.com>
2026-02-18 13:23:57 +0530

    dynamic values in the greeting message logic added

--- main.js
+++ main.js
@@ -1,7 +1,16 @@
-document.querySelector('#open-nav-menu').addEventListener('click', () => {
-  document.querySelector('header nav .wrapper').classList.add('nav-open');
-})
+//menu
+document.querySelector("#open-nav-menu").addEventListener("click", () => {
+  document.querySelector("header nav .wrapper").classList.add("nav-open");
+});
 
-document.querySelector('#close-nav-menu').addEventListener('click', () => {
-  document.querySelector('header nav .wrapper').classList.remove('nav-open');
-})
+document.querySelector("#close-nav-menu").addEventListener("click", () => {
+  document.querySelector("header nav .wrapper").classList.remove("nav-open");
+});
+//Greeting section
+const greetingText = "Good Afternoon";
+document.querySelector("#greeting").innerHTML = greetingText;
+const weatherCondition = "sunny";
+const userLocation = "New York";
+let temperature = 35.78;
+let weatherText = `The weather is ${weatherCondition} in ${userLocation} and the temperature is ${temperature.toFixed(2)}°C.`;
+document.querySelector("p#weather").innerHTML = weatherText;
