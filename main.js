176c5f73cd1c4f5f0fd72c3b97fecb28de38360f
Akansha Wajge <akanshad.wajge@gmail.com>
2026-02-18 22:15:05 +0530

    local time section logic added

--- main.js
+++ main.js
@@ -11,6 +11,27 @@ const greetingText = "Good Afternoon";
 document.querySelector("#greeting").innerHTML = greetingText;
 const weatherCondition = "sunny";
 const userLocation = "New York";
-let temperature = 35.78;
-let weatherText = `The weather is ${weatherCondition} in ${userLocation} and the temperature is ${temperature.toFixed(2)}°C.`;
-document.querySelector("p#weather").innerHTML = weatherText;
+let temperature = 30;
+let celsiusText = `The weather is ${weatherCondition} in ${userLocation} and the temperature is ${temperature.toFixed(2)}°C.`;
+let fahrText = `The weather is ${weatherCondition} in ${userLocation} and the temperature is ${celsiusToFahrenheit(temperature).toFixed(2)}°F.`;
+document.querySelector("p#weather").innerHTML = celsiusText;
+
+//Function to convert Celsius to Fahrenheit
+function celsiusToFahrenheit(celsius) {
+  return (celsius * 9) / 5 + 32;
+}
+document.querySelector(".weather-group").addEventListener("click", (event) => {
+  if (event.target.id === "celsius") {
+    document.querySelector("p#weather").innerHTML = celsiusText;
+  } else if (event.target.id === "fahrenheit") {
+    document.querySelector("p#weather").innerHTML = fahrText;
+  }
+
+})
+
+setInterval(() => {
+  const localTime = new Date();
+  document.querySelector("span[data-time='hours']").innerHTML = localTime.getHours().toString().padStart(2, "0");
+  document.querySelector("span[data-time='minutes']").innerHTML = localTime.getMinutes().toString().padStart(2, "0");
+  document.querySelector("span[data-time='seconds']").innerHTML = localTime.getSeconds().toString().padStart(2, "0");
+}, 1000);
